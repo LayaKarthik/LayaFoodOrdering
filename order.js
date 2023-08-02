8c697bf53f38ceadc8f4c8b8acdec72759bbc574
Laya Karthikeyan <61170495+LayaKarthik@users.noreply.github.com>
2023-08-02 00:05:49 -0700

    Update order.js

--- order.js
+++ order.js
@@ -1,1 +1,14 @@
+const getOrderById = (req,res, next) => {
+    Order.findById(req.params.orderId).select('-__v -items._id').exec((err, order) => {
+        if (err) {
+            // forward to express error handling middleware
+            return next(err);
+        }
+        res.status(200).json(order);
+    })
+}
 
+module.exports = {
+    placeOrder: placeOrder,
+    getOrderById: getOrderById
+}
