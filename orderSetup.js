62c98b49269a0eeaaa31a201b564d1274c34e3e4
Laya Karthikeyan <61170495+LayaKarthik@users.noreply.github.com>
2023-08-02 00:06:18 -0700

    Update orderSetup.js

--- orderSetup.js
+++ orderSetup.js
@@ -1,1 +1,10 @@
+const { orderSchema } = require('../models/orderModel');
+const mongoose = require('mongoose');
+const { changeOrderStatus } = require('../services/mongoService')
+const { ORDER_ACCEPTED, ORDER_DELIVERED } = require('../resources/constants')
 
+// Creating a model with Order as the object name and orderSchema as the Schema
+const OrderModel = mongoose.model('Order', orderSchema)
+
+// environment variables
+const ORDER_DELIVERY_TIME = parseInt(process.env.ORDER_DELIVERY_TIME) || 10000;
